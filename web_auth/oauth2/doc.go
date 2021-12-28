68b3c4c606b25ba9fc3207d989be7358a44f32e5
Marcel <marcel.hertel@gmx.net>
2021-12-28 10:17:08 +0100

    Move OAuth2 package docs to doc.go

---
+++ web_auth/oauth2/doc.go
@@ -0,0 +1,10 @@
+/*
+OAuth2 example using Authorization Code Grant Flow.
+
+Disclaimer
+
+Code is not production ready. Many things would still have to be improved.
+Current status served only for learning purposes.
+
+*/
+package main
