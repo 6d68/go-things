d7cbdaae24f41bf3ba1f7a0a7fb53caf6c0244c8
Marcel <marcel.hertel@gmx.net>
2021-12-31 15:34:45 +0100

    Move JWT  package docs to doc.go

---
+++ web_auth/jwt_cookie/doc.go
@@ -0,0 +1,13 @@
+/*
+Example shows a basic web app with user register, login and logout capabilities.
+A Token (JWT) is stored in cookie. Users and sessions are managed in-memory.
+If the server needs a restart, the cookies must be deleted manually for everything to work
+smoothly  again.
+
+Disclaimer
+
+Code is not production ready. Many things would still have to be improved.
+Current status served only for learning purposes.
+
+*/
+package main
